04c15f1c97d93c9b1fb50a88be93e6efb0de7d15
mubeenyasin72 <mubeenyasin75@gmail.com>
2025-04-11 19:28:51 +0500

    create controller of get food availability

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -1,19 +1,38 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { ApiResponse, ApiError, BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND  } from '../utility';
+import { asyncHandler, ApiResponse, ApiError, BAD_REQUEST, INTERNAL_SERVER_ERROR, NOT_FOUND, OK } from '../utility';
 import { Food, Vandor } from '../models';
 
-export const GetFoodAvailability = async (req: Request, res: Response, next: NextFunction) => {
 
-}
-export const GetTopRestaurants = async (req: Request, res: Response, next: NextFunction) => {
-    
-}
-export const GetFoodIn30Min = async (req: Request, res: Response, next: NextFunction) => {
-    
-}
-export const SearchFood = async (req: Request, res: Response, next: NextFunction) => {
-    
-}
-export const GetRestaurantById = async (req: Request, res: Response, next: NextFunction) => {
-    
-}
+
+export const GetFoodAvailability = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { pincode } = req.params;
+        const result = await Vandor.find({ pinCode: pincode, serviceAvailable: true })
+            .sort([['rating', -1]])
+            .populate("foods")
+        if (result.length > 0) {
+            return res.status(OK).json(new ApiResponse(OK, result, "Food Available"));
+        }
+        return res.status(NOT_FOUND).json(new ApiResponse(NOT_FOUND, [], "No Food Available"));
+    }
+)
+export const GetTopRestaurants = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+
+    }
+)
+export const GetFoodIn30Min = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+
+    }
+)
+export const SearchFood = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+
+    }
+)
+export const GetRestaurantById = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+
+    }
+)
--- routes/ShoppingRoute.ts
+++ routes/ShoppingRoute.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import { GetFoodAvailability, GetFoodIn30Min, GetRestaurantById, GetTopRestaurants, SearchFood } from "../controllers";
 
 const router = express.Router();
